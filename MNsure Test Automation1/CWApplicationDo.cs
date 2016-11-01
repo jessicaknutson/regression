2a73281fa67e303c05e38cbd63b7fb11c0560fa0
pwtfr01 <Timothy.Rawerts@state.mn.us>
2016-11-01 12:58:15 -0500

    TFR 11-01-2016 added code to deal with November 1st issue

--- MNsure Test Automation1/CWApplicationDo.cs
+++ MNsure Test Automation1/CWApplicationDo.cs
@@ -1644,6 +1644,12 @@ namespace MNsure_Regression_1
                 }
                  */
 
+                if (DateTime.Now > Convert.ToDateTime("10/31/2016") && DateTime.Now < Convert.ToDateTime("1/1/2017"))
+                {
+                    IWebElement listboxIncomeNextYear = driver.FindElement(By.Id("__o3id8"));
+                    listboxIncomeNextYear.SendKeys(incomeExpected);
+                }
+
                 writeLogs.DoGetScreenshot(driver, ref myHistoryInfo);
 
                 IWebElement buttonNext = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/a[2]/span/span/span"));
